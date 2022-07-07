5e86fd5327082f1c75c3a37121867f9bd57cf8ef
RynZzz-404 <108714745+RynZzz-404@users.noreply.github.com>
2022-07-07 20:52:02 +0800

    Add files via upload

--- message/help.js
+++ message/help.js
@@ -2,17 +2,17 @@ exports.txtSewabot = (prefix) => {
 return `
  1-Minggu : Rp5.000
  2-Minggu : Rp.10.000
- 1-Bulan : Rp.20.000
- 2-Bulan : Rp.30.000
- 3-Bulan : Rp40.000
+ 1-Bulan : Rp.15.000
+ 2-Bulan : Rp.20.000
+ 3-Bulan : Rp25.000
+ 𝙿𝚎𝚛𝚖𝚊𝚗𝚎𝚗𝚝 : 𝙱𝚒𝚔𝚒𝚗 𝙱𝚘𝚝 𝚂𝚎𝚗𝚍𝚒𝚛𝚒
 `
 }
 exports.txtDonate = (prefix) => {
 return `
 ▷_*DONASI BOT*_
- _- DANA : 08xxxxxxxx_
- _- OVO : 08xxxxxxxx_
- _- GOPAY : 08xxxxxxxx_
- _- QRIS : https://bit.ly/3yy0qoZ_
+ _- DANA : 082350496532_
+ _- PULSA (TELKOMSEL) : 082350496532_
+ _- GOPAY : 082252285143_
 `
 }
